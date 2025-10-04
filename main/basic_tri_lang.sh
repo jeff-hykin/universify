69092f3089af919497ee597306acc565f35deb3a
Jeff Hykin <jeff.hykin@gmail.com>
2025-10-04 17:38:54 -0500

    add reference

---
+++ main/basic_tri_lang.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+"\"",`$(echo --% ' |out-null)" >$null; <#${/*'>/dev/null )` 2>/dev/null;
+
+    echo "hi bash/sh"
+
+exit #>
+
+    echo "hi powershell"
+
+<# */0}`;
+    
+    console.log("Hello World", Deno.args)
+    
+// #>
