f243b01f33094f30b6a1f2fb73e22da55841a6b5
Jeff Hykin <jeff.hykin@gmail.com>
2025-10-06 12:22:34 -0500

    make shell installer easier to audit

--- main/2_readable.ps1
+++ main/2_readable.ps1
@@ -3,8 +3,10 @@
         echo "DENO_VERSION_HERE"; : --% ' |out-null <#';
     };
     # getDenoVersion exists as both a bash and powershell function so that DENO_VERSION_HERE is only mentioned in one place (no duplication)
+    # NOTE: semicolons at the end of lines are important for when this script is crushed into a one-liner
     deno_version="$(getDenoVersion)";
-    deno="$HOME/.deno/$deno_version/bin/deno";
+    DENO_INSTALL="$HOME/.deno/$deno_version";
+    deno="$DENO_INSTALL/bin/deno";
     target_script="$0";
     disable_url_run="DISABLE_URL_RUN_HERE";
     
@@ -26,47 +28,106 @@
     elif [ -f "$deno" ]; then 
         chmod +x "$deno" && exec "$deno" run UNIX_DENO_ARGS_HERE "$target_script" "$@";
     fi;
+    
     # 
-    # if the user doesn't have it, install deno
     # 
+    # if the user doesn't have the deno version, install it
+    # 
+    # 
+    
+    #
+    # prefix before official installer (increasing system compatibility)
+    #
+    
+    # deno needs curl, and either unzip or 7z
+    # however, wget can be used instead of curl,
+    # and on MacOS/OpenBSD tar can be used instead of unzip/7z
+    # and, if those are missing we can offer to install them.
+    # A universal script should be usable by newbies who don't know what apt-get is
+    
     has () {
         command -v "$1" >/dev/null;
     }; 
-    set -e;
     
-    # try to install unzip for the user if its missing
-    if ! has unzip && ! has 7z; then
-        echo "Can I try to install unzip for you? (its required for this command to work) ";read ANSWER;echo; 
-        if [ "$ANSWER" =~ ^[Yy] ]; then 
-            if ! has brew; then 
-                brew install unzip;
-            elif has apt-get; then
-                if [ "$(whoami)" = "root" ]; then 
-                    apt-get install unzip -y;
-                elif has sudo; then 
-                    echo "I'm going to try sudo apt install unzip";read ANSWER;echo; 
-                    sudo apt-get install unzip -y; 
-                elif has doas; then 
-                    echo "I'm going to try doas apt install unzip";read ANSWER;echo; 
-                    doas apt-get install unzip -y; 
-                else apt-get install unzip -y; 
-                fi; 
-            fi; 
-        fi; 
-        
-        # if still doesn't have unzip somehow
-        if ! has unzip; then 
-            echo "";
-            echo "So I couldn't find an 'unzip' command";
-            echo "And I tried to auto install it, but it seems that failed";
-            echo "(This script needs unzip and either curl or wget)";
-            echo "Please install the unzip command manually then re-run this script";
-            exit 1; 
-        fi; 
+    # using wget as curl
+    if ! has curl; then
+        if ! has wget; then
+            curl () {
+                # use $5 and $6 because deno uses curl below as:
+                #      curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri";
+                wget --output-document="$5" "$6";
+            };
+        else
+            echo "Sorry this script needs curl or wget, please install one or the other and re-run";
+            exit 1;
+        fi;
     fi;
     
+    # using MacOS/OpenBSD's tar command as unzip
+    if [ "$(uname)" = "Darwin" ]; then
+        unzip () {
+            # use $4 and $2 because deno uses unzip below as:
+            #      unzip -d "$bin_dir" -o "$exe.zip"
+            /usr/bin/tar xvf "$4" -C "$2";
+        };
+    fi;
     
+    # check for an unzip tool (most common problem)
     if ! has unzip && ! has 7z; then
+        echo "Either the unzip or 7z command are needed for this script";
+        echo "Should I try to install unzip for you?";read ANSWER;echo;
+        if [ "$ANSWER" =~ ^[Yy] ]; then
+            # if nix is available, use it becuase it won't require modifying the user's environment or sudo
+            if has nix-shell; then
+                # this gets unzip without modifying the user's environment
+                unzip_path="$(NIX_PATH='nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-25.05.tar.gz' nix-shell -p unzip which --run "which unzip")"
+                alias unzip="$unzip_path"
+            else
+                # select the base install command (all of these need root by default)
+                if has apt-get; then
+                    _install="apt-get install unzip -y"
+                elif has dnf; then
+                    _install="dnf install unzip -y"
+                elif has pacman; then
+                    _install="pacman -S --noconfirm unzip"
+                else
+                    echo "Sorry, I don't know how to install unzip on this system";
+                    echo "Please install unzip manually and re-run this script";
+                    exit 1;
+                fi;
+                
+                # run the install command according to context/user
+                if [ "$(whoami)" = "root" ]; then 
+                    # Note: many systems (ex: docker) default to root user
+                    # trying to use sudo as root will usually cause "sudo command not found"
+                    "$_install";
+                elif has sudo; then 
+                    sudo "$_install"; 
+                elif has doas; then 
+                    doas "$_install"; 
+                else
+                    # just try, even if no sudo/doas
+                    "$_install"; 
+                fi;
+            fi;
+        fi;
+        
+        # if still doesn't have unzip somehow
+        if ! has unzip; then
+            echo "";
+            echo "So I couldn't find an 'unzip' or '7z' command";
+            echo "And I tried to auto install unzip, but it seems that failed";
+            echo "Please install the unzip or 7z command manually then re-run this script";
+            exit 1; 
+        fi;
+    fi;
+    
+    #
+    # the official deno installer with parts commented out
+    #
+    set -e;
+
+    if ! command -v unzip >/dev/null && ! command -v 7z >/dev/null; then
         echo "Error: either unzip or 7z is required to install Deno (see: https://github.com/denoland/deno_install#either-unzip-or-7z-is-required )." 1>&2;
         exit 1;
     fi;
@@ -82,62 +143,57 @@
         esac
     fi;
 
-    print_help_and_exit() {
-        echo "Setup script for installing deno
+    # Arg-parsing commended out because all args are passed to script (stuff like --help should go to the script)
+    
+    # print_help_and_exit() {
+    #     echo "Setup script for installing deno
 
-    Options:
-    -y, --yes
-        Skip interactive prompts and accept defaults
-    --no-modify-path
-        Don't add deno to the PATH environment variable
-    -h, --help
-        Print help
-    "
-        echo "Note: Deno was not installed";
-        exit 0;
-    };
+    # Options:
+    # -y, --yes
+    #     Skip interactive prompts and accept defaults
+    # --no-modify-path
+    #     Don't add deno to the PATH environment variable
+    # -h, --help
+    #     Print help
+    # "
+    #     echo "Note: Deno was not installed";
+    #     exit 0;
+    # };
+    #
+    # # Simple arg parsing - look for help flag, otherwise
+    # # ignore args starting with '-' and take the first
+    # # positional arg as the deno version to install
+    # for arg in "$@"; do
+    #     case "$arg" in
+    #     "-h")
+    #         print_help_and_exit
+    #         ;;
+    #     "--help")
+    #         print_help_and_exit
+    #         ;;
+    #     "-"*) ;;
+    #     *)
+    #         if [ -z "$deno_version" ]; then
+    #             deno_version="$arg"
+    #         fi
+    #         ;;
+    #     esac
+    # done
+    # if [ -z "$deno_version" ]; then
+    #     deno_version="$(curl -s https://dl.deno.land/release-latest.txt)"
+    # fi
 
-    # Simple arg parsing - look for help flag, otherwise
-    # ignore args starting with '-' and take the first
-    # positional arg as the deno version to install
-    for arg in "$@"; do
-        case "$arg" in
-        "-h")
-            print_help_and_exit
-            ;;
-        "--help")
-            print_help_and_exit
-            ;;
-        "-"*) ;;
-        *)
-            if [ -z "$deno_version" ]; then
-                deno_version="$arg";
-            fi
-            ;;
-        esac
-    done;
-    if [ -z "$deno_version" ]; then
-        deno_version="$(curl -s https://dl.deno.land/release-latest.txt)";
-    fi;
-
-    deno_uri="https://dl.deno.land/release/v${deno_version}/deno-${target}.zip";
-    deno_install="${DENO_INSTALL:-$HOME/.deno/$deno_version}";
+    deno_uri="https://dl.deno.land/release/${deno_version}/deno-${target}.zip";
+    deno_install="${DENO_INSTALL:-$HOME/.deno}";
     bin_dir="$deno_install/bin";
     exe="$bin_dir/deno";
 
     if [ ! -d "$bin_dir" ]; then
         mkdir -p "$bin_dir";
     fi;
-    
-    if has curl; then
-        curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri";
-    elif has wget; then
-        wget --output-document="$exe.zip" "$deno_uri";
-    else
-        echo "Error: curl or wget is required to download Deno (see: https://github.com/denoland/deno_install )." 1>&2;
-    fi;
-    
-    if has unzip; then
+
+    curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri";
+    if command -v unzip >/dev/null; then
         unzip -d "$bin_dir" -o "$exe.zip";
     else
         7z x -o"$bin_dir" -y "$exe.zip";
@@ -145,35 +201,38 @@
     chmod +x "$exe";
     rm "$exe.zip";
     
-    # run self with deno (exec takes over, so no need to Exit)
-    exec "$deno" run UNIX_DENO_ARGS_HERE "$0" "$@";
-    
-    
     # commented out below because we don't want to modify the user's env/path (no side effects)
-    
-    # echo "Deno was installed successfully to $exe";
+
+    # echo "Deno was installed successfully to $exe"
 
     # run_shell_setup() {
-    #     "$exe" run -A --reload "https://esm.sh/jsr/@deno/installer-shell-setup@0.3.1/bundled.esm.js" --yes --no-modify-path "$deno_install" "$@";
-    # };
+    #     $exe run -A --reload jsr:@deno/installer-shell-setup/bundled "$deno_install" "$@"
+    # }
 
     # # If stdout is a terminal, see if we can run shell setup script (which includes interactive prompts)
     # if [ -z "$CI" ] && [ -t 1 ] && $exe eval 'const [major, minor] = Deno.version.deno.split("."); if (major < 2 && minor < 42) Deno.exit(1)'; then
     #     if [ -t 0 ]; then
-    #         run_shell_setup "$@";
+    #         run_shell_setup "$@"
     #     else
     #         # This script is probably running piped into sh, so we don't have direct access to stdin.
     #         # Instead, explicitly connect /dev/tty to stdin
-    #         run_shell_setup "$@" </dev/tty;
+    #         run_shell_setup "$@" </dev/tty
     #     fi
-    # fi;
+    # fi
     # if command -v deno >/dev/null; then
-    #     echo "Run 'deno --help' to get started";
+    #     echo "Run 'deno --help' to get started"
     # else
-    #     echo "Run '$exe --help' to get started";
-    # fi;
-    # echo;
-    # echo "Stuck? Join our Discord https://discord.gg/deno";
+    #     echo "Run '$exe --help' to get started"
+    # fi
+    # echo
+    # echo "Stuck? Join our Discord https://discord.gg/deno"
+    
+    #
+    # end of deno installer
+    #
+    
+    # run self with deno (exec takes over, so no need to Exit)
+    exec "$deno" run UNIX_DENO_ARGS_HERE "$0" "$@";
 
     # 
     # powershell portion
